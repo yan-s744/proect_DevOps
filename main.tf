c65921db243d4a98f2f17b1f495c6a972cec7adb
Андрей <gaconin@yandex.ru>
2026-01-09 09:19:32 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
- resource "yandex_compute_instance" "virtual_machine" {
+resource "yandex_compute_disk" "boot-disk" {
+  for_each = var.virtual_machines
+  name     = each.value["disk_name"]
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  size     = each.value["disk"]
+  image_id = each.value["template"]
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+name = "subnet1"
+zone = "ru-central1-a"
+network_id = yandex_vpc_network.network-1.id
+v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_compute_instance" "virtual_machine" {
   for_each        = var.virtual_machines
   name = each.value["vm_name"]
 
